3208a2f39da99e2cf832ba5b80d28f5f3e337167
t89 <toshiharutf@gmail.com>
2025-03-31 21:37:19 +0400

    added toshi toolchain

---
+++ toshi_toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Generic)
+
+set(CMAKE_VERBOSE_MAKEFILE ON)
+
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+set(CMAKE_C_COMPILER   "/home/t89/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gcc")
+set(CMAKE_CXX_COMPILER "/home/t89/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-g++")
+set(CMAKE_ASM_COMPILER "/home/t89/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-gcc")
+set(BINARY_COPY "/home/t89/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-objcopy")
+set(BINARY_SIZE "/home/t89/gcc-arm-none-eabi-8-2018-q4-major/bin/arm-none-eabi-size")
+
+set(IS_EMBEDDED TRUE)
